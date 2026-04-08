7d290ffa30929df3cce888588d1dc5c20f6a732a
Pepocero <pepocero@hotmail.com>
2026-04-08 20:29:55 +0200

    Arreglo pagina en blanco tras estar inactiva

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,14 @@ import './theme-tokens.css'
 import './index.css'
 import App from './App.tsx'
 
+/**
+ * Tras un despliegue, un tab antiguo puede intentar cargar chunks con hash viejo.
+ * Vite emite `vite:preloadError`; recargar trae el nuevo index+assets.
+ */
+window.addEventListener('vite:preloadError', () => {
+  window.location.reload()
+})
+
 initThemeFromStorage()
 
 createRoot(document.getElementById('root')!).render(
